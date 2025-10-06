e38c7d6c98f02b1d8f5ab54a6b2ce2d3bafcb347
alperosci <alperosci@gmail.com>
2025-10-06 21:38:50 +0300

    some decoration

--- src/main.rs
+++ src/main.rs
@@ -21,10 +21,10 @@ fn main() {
     let mut shownftxt: String = String::new();
     let mut addtologtxt: String = String::new();
 
-    getinp(&mut url, "Enter URL to watch : ");
-    getinp(&mut durationtxt, "Enter time to wait between requests (seconds) : ");
-    getinp(&mut shownftxt, "Do you want to get notified when cannot find? (y for yes, anything for no) : ");
-    getinp(&mut addtologtxt, "Do you want log? (y for yes, anything for no) : ");
+    getinp(&mut url, "-Enter URL to watch : ");
+    getinp(&mut durationtxt, "-Enter time to wait between requests (seconds) : ");
+    getinp(&mut shownftxt, "-Do you want to get notified when cannot find? (y for yes, anything for no) : ");
+    getinp(&mut addtologtxt, "-Do you want log? (y for yes, anything for no) : ");
 
     let duration_secs: u64 = durationtxt.parse().unwrap();
     let duration: Duration = Duration::from_secs(duration_secs);
@@ -47,9 +47,9 @@ fn main() {
         let site: String = get(&url).unwrap().text().unwrap();
 
         if site != oldsite {
-            println!("##### A change detected! - time : {} #####",Local::now());
+            println!("----- A change detected! -> time : {} -----",Local::now());
             if addtolog {
-                writeln!(logfile,"##### A change detected! - time : {} #####",Local::now()).expect("Error: Cannot write to log ...");
+                writeln!(logfile,"----- A change detected! -> time : {} -----",Local::now()).expect("Error: Cannot write to log ...");
             }
             oldsite = site;
         } else if showft {
